cf44cab7968c8af7d7a9dc77ad235d4462e61e6a
Julie Zhu <juliez@princeton.edu>
2016-07-27 15:57:59 -0500

    Add testing for spam svm

--- spam_train.py
+++ spam_train.py
@@ -1,7 +1,8 @@
 import numpy as np
+import scipy.io as sio
 from sklearn.svm import SVC
 
-import process_email as pe #?
+#from process_email import email_features, process_email
 
 # Trains svm
 # email_features should be 2d array, classification is simple list
@@ -13,9 +14,20 @@ def spam_train(email_features, classification):
 	model.fit(X, y)
 	SVC(kernel = "linear") # need to read up on them parameters
 
-	#print model.predict([some appropriately long 2d array]) 
+	# accuracy on training data, probs not best practice
+	print model.score(X, y)
+
+	# accuracy on test data, better practice lel
+	# this score is lower than the pdf predicted....hmmm
+	test_mat = sio.loadmat('spamTest.mat')
+	Xtest = test_mat['Xtest']
+	ytest = np.ravel(test_mat['ytest'])
+	print model.score(Xtest, ytest)
 
 # for testing
 if __name__ == "__main__":
     import sys
-    spam_train(sys.argv[1])
+    mat_contents = sio.loadmat('spamTrain.mat')
+    X = mat_contents['X']
+    y = np.ravel(mat_contents['y'])
+    spam_train(X, y)
