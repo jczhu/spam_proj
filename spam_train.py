3c2d74066b03b0392710b534587875b70c86a884
Julie Zhu <juliez@princeton.edu>
2016-07-28 22:34:07 -0500

    Add code returning features most indicative of spam

--- spam_train.py
+++ spam_train.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy.io as sio
 from sklearn.svm import SVC
 
-#from process_email import email_features, process_email
+from process_email import get_vocab_list
 
 # Trains svm
 # email_features should be 2d array, classification is simple list
@@ -24,10 +24,21 @@ def spam_train(email_features, classification):
 	ytest = np.ravel(test_mat['ytest'])
 	print model.score(Xtest, ytest)
 
+	return model
+
+# Returns 15 words most common (indicative?) of spam
+# Can only use if linear svm model
+def top_spam_indicators(model):
+	coef = np.array(np.ravel(model.coef_))
+	return np.argpartition(coef, -15)[-15:]
+
 # for testing
 if __name__ == "__main__":
     import sys
     mat_contents = sio.loadmat('spamTrain.mat')
     X = mat_contents['X']
     y = np.ravel(mat_contents['y'])
-    spam_train(X, y)
+    model = spam_train(X, y)
+    tsi = top_spam_indicators(model)
+    vocab_list = get_vocab_list("vocab.txt")
+    print [(vocab_list[x]) for x in tsi] # also getting different results here
