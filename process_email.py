bd732d531408ecfc896ce2a7d7b7b78c16870639
Julie Zhu <juliez@princeton.edu>
2016-08-06 21:33:43 -0500

    Fix issue processing urls.

--- process_email.py
+++ process_email.py
@@ -1,5 +1,6 @@
 from nltk import PorterStemmer
 import re
+import string
 
 # Reads in vocab list from filename and convert to python list.
 # Assumes vocab list file separates vocab by new line. May have to change.
@@ -30,7 +31,7 @@ def process_email(email_contents):
 	email_contents = re.sub(r"[0-9]+", "number", email_contents)
 
 	# handle urls
-	email_contents = re.sub(r"(http|https)://[^\S]*", "httpaddr", email_contents)
+	email_contents = re.sub(r"(http|https)://[\S]*", "httpaddr", email_contents)
 
 	# handle email addresses
 	# Original Coursera just looks for @, so I amped it up a bit by looking for period
@@ -39,13 +40,15 @@ def process_email(email_contents):
 	# handle dollar sign
 	email_contents = re.sub(r"[$]+", "dollar", email_contents)
 
-	# after doing all that, remove all non alphanumeric characters
+	# after doing all that, strip punctuation
 	email_contents = re.sub(r"[^a-zA-Z0-9]", " ", email_contents)
 
 	# Tokenize email and convert individual words to numbers, based on vocab list
 	tokens = email_contents.split()
 	for i in range(0, len(tokens)):
 		tokens[i] = PorterStemmer().stem_word(tokens[i])
+
+		print tokens[i],
 		try:
 			temp = vocab_list.index(tokens[i])
 		except ValueError:
@@ -76,6 +79,8 @@ def email_features(word_indices):
 
 
 # for testing
-#if __name__ == "__main__":
-#    import sys
-#    email_features(process_email(sys.argv[1]))
+if __name__ == "__main__":
+   import sys
+   with open(sys.argv[1], 'r') as myfile: 
+   	email_contents = myfile.read().replace('\n', ' ')
+   process_email(email_contents)
