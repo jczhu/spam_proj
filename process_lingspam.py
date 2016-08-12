7794357f86e05d9cda37732cf99d42c7da26f31d
Julie Zhu <juliez@princeton.edu>
2016-08-11 22:45:02 -0500

    Fixed email processing, attempt to add method to find most frequently occuring words

--- process_lingspam.py
+++ process_lingspam.py
@@ -1,29 +1,55 @@
 from nltk import PorterStemmer
 from nltk.corpus import stopwords
+from operator import itemgetter
 import re
 import string
 import os
 
 from process_email import process_email, email_features
 
-# holds the parsed email
-all_processed = []
-
 def process_lingspam():
 	all_paths = ['/part1/', '/part2/', '/part3/', '/part4/', '/part5/', '/part6/', 
 	'/part7/', '/part8/', '/part9/', '/part10/']
+
+	# will hold all the processed emails
+	all_processed = []
+
 	for path in all_paths:
 		with cd(os.getcwd() + path):
-			process_folder()
+			all_processed = process_folder(all_processed)
+
+	# remove the first empty element and return
+	return all_processed[1:]
 
 # remove subject line headers from each file and process the body of the email
 # return all the processed results from current folder
-def process_folder():
-	global all_processed
+# email_list is the list to add processed emails to
+def process_folder(email_list):
 	for filename in os.listdir(os.getcwd()):
 		with open(filename, 'r') as myfile:
 			email_contents = myfile.read().splitlines(True)
-			all_processed.append(process_email(' '.join(email_contents[2:])))
+			email_list.append(process_email(' '.join(email_contents[2:])))
+	
+	return email_list
+
+# finds most common words in processed emails
+# processed_emails is the list of lists of processed emails
+def common_words(processed_emails):
+	# create dictionary of all the word stems in the processed emails
+	word_dict = {}
+	for i in range(0, len(processed_emails)):
+		for word in processed_emails[i]:
+			if not word in word_dict:
+				word_dict[word] = 1
+			else:
+				word_dict[word] += 1
+
+	# sort the words in order of frequency (most frequent to least)
+	word_list = word_dict.items()
+	word_list = sorted(word_list, key=itemgetter(1))
+
+	# return first 2000 tuples
+	return word_list[:2000]
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -44,7 +70,8 @@ if __name__ == "__main__":
 	# clean emails for each folder in lingspam
 	path = "~/Downloads/lingspam_public/bare/" 
 	with cd(path):
-		process_lingspam()
-	print all_processed[0]
+		all_processed = process_lingspam()
+	print len(all_processed)
+	print common_words(all_processed)
 
 
